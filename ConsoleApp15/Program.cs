866d48f2656aeab6be017cca9d1676e00c6e55b6
Bingo <Bingo@DUSAN3>
2023-10-08 18:49:27 +0200

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,15 @@
+﻿// https://sr.wikipedia.org/wiki/C_Sharp#%D0%9A%D0%BB%D0%B0%D1%81%D0%B5
+// https://learn.microsoft.com/en-us/visualstudio/get-started/csharp/tutorial-console?view=vs-2022
+// https://git-scm.com/
+// https://github.com/draganilicnis
+// https://petlja.org/biblioteka/r/Zbirka/01%20Aritmetika/01%20Formule/01%20geometrijske%20formule
+// https://onlinegdb.com/MNLD9LcCA
+
+using System;
+class Zadatak
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Proba");
+    }
+}
